abda0fc08f8cdd4f795c943f6997b496bb77f053
bublik <bublik.script@gmail.com>
2023-07-22 18:00:14 +0300

    Scene init

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import dynamic from 'next/dynamic'
 
-const AblyChatComponent = dynamic(
-  () => import('@/components/AblyChatComponent'),
-  { ssr: false },
-)
+const Playground = dynamic(() => import('@/components/Playground'), {
+  ssr: false,
+})
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <AblyChatComponent />
+      <Playground />
     </main>
   )
 }
---
+++ components/Playground.tsx
@@ -0,0 +1,182 @@
+'use client'
+
+import React, {
+  forwardRef,
+  Suspense,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react'
+import {
+  Canvas,
+  extend,
+  Object3DNode,
+  useFrame,
+  useThree,
+} from '@react-three/fiber'
+import * as THREE from 'three'
+import {
+  BoxGeometry,
+  DoubleSide,
+  Mesh,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  SphereGeometry,
+  Vector3,
+} from 'three'
+
+class VelocityMesh extends Mesh {
+  velocity: Vector3 = new Vector3(0, 0, 0.1)
+}
+
+// Add types to ThreeElements elements so primitives pick up on it
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    velocityMesh: Object3DNode<VelocityMesh, typeof VelocityMesh>
+  }
+}
+
+extend({ VelocityMesh })
+
+// Component for Plane
+const Plane = forwardRef<Mesh | null>((props, ref) => {
+  const mesh = useRef<Mesh | null>(null)
+
+  useImperativeHandle(ref, () => mesh.current as Mesh, [mesh.current])
+
+  useEffect(() => {
+    const handlePlaneOrientation = (event: DeviceOrientationEvent) => {
+      var alpha = event.alpha || 0
+      var beta = event.beta || 0
+      var gamma = event.gamma || 0
+
+      // rotate the plane based on device orientation
+      mesh.current?.rotation.set(
+        (beta * Math.PI) / 180,
+        (alpha * Math.PI) / 180,
+        (-gamma * Math.PI) / 180,
+      )
+    }
+
+    window.addEventListener('deviceorientation', handlePlaneOrientation)
+
+    return () =>
+      window.removeEventListener('deviceorientation', handlePlaneOrientation)
+  }, [])
+
+  return (
+    <mesh
+      ref={mesh}
+      geometry={new PlaneGeometry(1, 2)}
+      material={new MeshBasicMaterial({ color: 0x00aa00, side: DoubleSide })}
+    />
+  )
+})
+
+// Component for Wall
+const Wall = forwardRef<Mesh | null>((props, ref) => {
+  return (
+    <mesh
+      ref={ref}
+      position={[0, 0, -5]}
+      geometry={new BoxGeometry(10, 10, 1)}
+      material={new MeshBasicMaterial({ color: 0xff0000 })}
+    />
+  )
+})
+
+interface BallProps {
+  position: [number, number, number]
+  onUpdate: (mesh: VelocityMesh | null) => void
+}
+
+// Component for Ball
+const Ball: React.FC<BallProps> = (props) => {
+  const mesh = useRef<VelocityMesh | null>(null)
+  const [velocity, setVelocity] = useState(new Vector3(0, 0, 0.1))
+
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.position.add(velocity)
+      props.onUpdate(mesh.current)
+    }
+  })
+
+  return (
+    <velocityMesh
+      ref={mesh}
+      {...props}
+      geometry={new SphereGeometry(0.2, 32, 32)}
+      material={new MeshBasicMaterial({ color: 0x0000ff })}
+    />
+  )
+}
+
+// Main scene
+const Scene: React.FC = () => {
+  const [balls, setBalls] = useState<number[]>([])
+  const { scene } = useThree()
+  const plane = useRef<Mesh>(null)
+  const wall = useRef<Mesh>(null)
+
+  // Fires a ball every 2 seconds
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const id = Math.random()
+      setBalls((balls) => [...balls, id])
+      setTimeout(() => {
+        setBalls((balls) => balls.filter((ballId) => ballId !== id))
+      }, 5000)
+    }, 2000)
+
+    return () => clearInterval(intervalId)
+  }, [])
+
+  // Handles collision with plane and wall
+  const handleCollision = (mesh: VelocityMesh | null) => {
+    if (mesh && plane.current && wall.current) {
+      const planeBox = new THREE.Box3().setFromObject(plane.current)
+      // const wallBox = new THREE.Box3().setFromObject(wall.current)
+      const ballSphere = new THREE.Sphere(mesh.position, 0.2)
+
+      if (planeBox.intersectsSphere(ballSphere)) {
+        console.log(plane.current.quaternion)
+        let normal = new Vector3(0, 0, 1)
+        normal.applyQuaternion(plane.current.quaternion)
+        mesh.velocity.reflect(normal)
+      }
+
+      // if (wallBox.intersectsSphere(ballSphere)) {
+      //   let normal = new Vector3(0, 0, -1)
+      //   normal.applyQuaternion(wall.current.quaternion)
+      //   mesh.velocity.reflect(normal)
+      // }
+    }
+  }
+
+  return (
+    <>
+      <Plane ref={plane} />
+      <Wall ref={wall} />
+      {balls.map((id) => (
+        <Ball key={id} position={[0, 0, -4.8]} onUpdate={handleCollision} />
+      ))}
+    </>
+  )
+}
+
+// Canvas component
+const Playground: React.FC = () => (
+  <Canvas
+    style={{ position: 'fixed', inset: '0', pointerEvents: 'none' }}
+    camera={{ position: [-5, 5, 5], fov: 75 }}
+  >
+    <color attach="background" args={[0, 0, 0]} />
+    <Suspense fallback={null}>
+      <Scene />
+    </Suspense>
+  </Canvas>
+)
+
+export default Playground
