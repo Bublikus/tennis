f7db73f9aea9781e33779749713d40366d409e7f
bublik <bublik.script@gmail.com>
2023-07-26 21:11:14 +0300

    Refactor Playground component to use @splinetool/react-spline and @splinetool/runtime.

--- components/Playground/Playground.tsx
+++ components/Playground/Playground.tsx
@@ -1,27 +1,45 @@
 'use client'
 
-import { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import Spline, { SPEObject } from '@splinetool/react-spline'
 import { useChannel } from '@/hooks/useChannel'
 import { throttle } from '@/utils/misc'
 import { OrientationData } from '@/types/orientation'
 import { CHANNEL_NAME } from '@/constants/channel'
-import PlaygroundScene from './PlaygroundScene'
+import { Application } from '@splinetool/runtime'
 
 export type PlaygroundSceneRef = {
   handlePlaneOrientation(event: DeviceOrientationEvent): void
 }
 
 export default function Playground() {
-  const sceneRef = useRef<PlaygroundSceneRef>(null)
+  const racket = useRef<SPEObject>()
 
   const [isMobile, setIsMobile] = useState(false)
   const [isPermissionGranted, setIsPermissionGranted] = useState(false)
 
+  const onLoad = (spline: Application) => {
+    racket.current = spline.findObjectByName('racket')
+  }
+
+  const handleRacketOrientation = useCallback((event: OrientationData) => {
+    if (!racket.current) return
+
+    const alpha = event.alpha ?? 0
+    const beta = event.beta ?? 0
+    const gamma = event.gamma ?? 0
+
+    const degreesToRadians = (degrees: number) => degrees * (Math.PI / 180)
+
+    racket.current.rotation.x = degreesToRadians(beta)
+    racket.current.rotation.y = degreesToRadians(alpha)
+    racket.current.rotation.z = degreesToRadians(-gamma)
+  }, [])
+
   const [channel] = useChannel(CHANNEL_NAME, (event) => {
     if (!event.data) return
 
-    // Display data on screen
-    sceneRef.current?.handlePlaneOrientation(event.data)
+    handleRacketOrientation(event.data)
   })
 
   const throttledPublish = useCallback(
@@ -36,20 +54,18 @@ export default function Playground() {
     (event: DeviceOrientationEvent) => {
       if (!event) return
 
-      if (isMobile) {
-        // Send data to server
-        let data: OrientationData = {
-          alpha: event.alpha ?? 0,
-          beta: event.beta ?? 0,
-          gamma: event.gamma ?? 0,
-        }
-        throttledPublish(data)
+      let data: OrientationData = {
+        alpha: event.alpha ?? 0,
+        beta: event.beta ?? 0,
+        gamma: event.gamma ?? 0,
       }
 
-      // Display data on screen
-      sceneRef.current?.handlePlaneOrientation(event)
+      // Send data to server
+      if (isMobile) throttledPublish(data)
+
+      handleRacketOrientation(data)
     },
-    [throttledPublish, isMobile],
+    [throttledPublish, handleRacketOrientation, isMobile],
   )
 
   const onStartRacket = useCallback(() => {
@@ -71,7 +87,10 @@ export default function Playground() {
   return (
     <div className="flex-auto flex flex-col items-center justify-center">
       {(!isMobile || (isMobile && isPermissionGranted)) && (
-        <PlaygroundScene ref={sceneRef} />
+        <Spline
+          scene="https://prod.spline.design/ZSbFjFdOI42Ok8vC/scene.splinecode"
+          onLoad={onLoad}
+        />
       )}
 
       {isMobile && !isPermissionGranted && (
